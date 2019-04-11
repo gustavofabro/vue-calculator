c2573f83724204ea2ae7b10c27b80c17e86b2fdb
Gustavo <gustavofabro.f@gmail.com>
2019-04-10 22:34:25 -0300

    calculator functions

--- src/main/Calculator.vue
+++ src/main/Calculator.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="calculator">
-        <Display value="111"/>
+        <Display :value="displayValue"/>
         <Button label="AC" triple @onClick="clearMemory"/>
         <Button label="/" operation @onClick="setOperation"/>
         <Button label="7" @onClick="addDigit"/>
@@ -26,13 +26,75 @@
     import Button from  "../components/Button"
     
     export default {
+        data: function () {
+            return {
+                displayValue: "0",
+                clearDisplay: false,
+                operation: null,
+                values: [0, 0],
+                current: 0
+            }
+        },
         components: { Button, Display },
         methods: {
             clearMemory() {
+                Object.assign(this.$data, this.$options.data())
             },
-            setOperation() {
+            setOperation(operation) {
+                if (this.current === 0) {
+                    this.operation = operation
+                    this.current = 1
+                    this.clearDisplay = true
+                } else {
+                    const equals = operation === "="
+                    const currentOperation = this.operation
+
+                    this.values[0] = this.calculateValues(this.values, currentOperation)
+                    this.values[1] = 0
+
+                    this.displayValue = this.values[0]
+                    this.operation = equals? null : operation
+                    this.current = equals? 0 : 1
+                    this.clearDisplay = !equals
+               }
             },
-            addDigit() {
+            addDigit(n) {
+                if (n == '.' && this.displayValue.includes('.')) {
+                    return;
+                }
+
+                const clearDisplay = this.displayValue === "0"
+                    || this.clearDisplay
+
+                const currentValue = clearDisplay? "" : this.displayValue
+                const displayValue = currentValue + n
+
+                this.displayValue = displayValue
+                this.clearDisplay = false
+                this.values[this.current] = displayValue
+            },
+
+            calculateValues(values, operation) {
+                let result = 0
+
+                switch (operation) {
+                    case '+':
+                        result = values[0] + values[1]
+                        break
+                    case '-':
+                        result = values[0] - values[1]
+                        break
+                    case '/':
+                        result = values[0] / values[1]
+                        break
+                    case '*':
+                        result = values[0] * values[1]
+                        break
+                    default:
+                        break
+                }
+
+                return result
             }
         }
     }
